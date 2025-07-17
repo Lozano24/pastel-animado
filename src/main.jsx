64d9b4c9fd34fd01423a3ef6ce942f15fefbcdc4
migue.net2010@gmail.com <Lozano24>
2025-07-16 20:35:13 -0500

    🎂 Primer commit del pastel animado

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AnimatedCake from './AnimatedCake'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AnimatedCake />
+  </React.StrictMode>
+)
